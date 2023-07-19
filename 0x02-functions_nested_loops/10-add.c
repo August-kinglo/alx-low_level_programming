cae7245a2441c7187beaffd2ba7982aaa0227a23
August Abdon KINGLO <113606354+August-kinglo@users.noreply.github.com>
2023-07-19 18:12:53 +0200

    Update 10-add.c

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -1,27 +1,15 @@
-#include <stdio.h>
-void times_table(void)
-{
-	int a, b, c;
+#include "holberton.h"
 
-	for (a = 0; a <= 9; a++)
-	{
-		_putchar(48);
-		for (b = 1; b <= 9; b++)
-		{
-			c = a * b;
-			_putchar(44);
-			_putchar(32);
-			if (c <= 9)
-			{
-				_putchar(32);
-				_putchar(c + 48);
-			}
-			else
-			{
-				_putchar((c / 10) + 48);
-				_putchar((c % 10) + 48);
-			}
-		}
-	_putchar('\n');
-	}
+/**
+* add - a function that adds two integers
+* and returns the result.
+*
+* @a: number one.
+* @b: number two.
+*
+* Return: Add of number one and number two.
+*/
+int add(int a, int b)
+{
+	return (a + b);
 }
